a61f1210fcce86285ccb65822b6ca2544058d5c9
Tmrvarun <er.varuntomar@gmail.com>
2025-07-12 16:39:37 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-pytest -s -v -m "sanity" --html=./Reports/report1.html testcases/ --browser chrome
+pytest -s -v -m "sanity" --browser chrome
 rem pytest -s-v-m "sanity and regression" --html=./Reports/report2.html testcases/ --browser chrome
 rem pytesr -s-v-m "regression" --html=./Reports/report3.html testcases/ --browser chrome
